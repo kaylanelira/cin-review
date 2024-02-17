d5db690d338354ba8b2ff8ac1e2f4c593632edfe
Thiago Alves <thijalves@gmail.com>
2024-02-17 14:28:21 -0300

    feat(review): implement test for duplicated reviews

--- backend/src/tests/features/review.feature
+++ backend/src/tests/features/review.feature
@@ -5,3 +5,9 @@ Scenario: Cadastrar um review com sucesso
     When uma requisição POST é enviada "/review/add" com username "TJAS", disciplina "Física", nota "10" e comentário "Gostei muito"
     Then o código da resposta é "200"
     And o JSON da resposta deve conter username "TJAS", disciplina "Física", nota "10" e comentário "Gostei muito"
+
+Scenario: Cadastrar um review repetido
+    Given o usuário "TJAS" já tem um review cadastrado com disciplina "Física", nota "10" e comentário "Gostei muito"
+    When uma requisição POST é enviada "/review/add" com username "TJAS", disciplina "Física", nota "10" e comentário "Gostei muito"
+    Then o código da resposta é "409"
+    And o JSON da resposta deve conter a mensagem "User has already reviewed this discipline"
--- backend/src/tests/step-definitions/test_review.py
+++ backend/src/tests/step-definitions/test_review.py
@@ -45,3 +45,47 @@ def response_json(context, username: str, discipline: str, comment: str):
     assert response_json["discipline"] == discipline
     assert response_json["rating"] == 10
     assert response_json["comment"] == comment
+
+@scenario(scenario_name='Cadastrar um review repetido', feature_name='../features/review.feature')
+def test_cadastrar_um_review_repetido():
+    pass
+
+@given(
+    parsers.cfparse('o usuário "{username}" já tem um review cadastrado com disciplina "{discipline}", nota "{rating}" e comentário "{comment}"')
+)
+def add_review(client, username: str, discipline: str, rating: int, comment: str):
+    review = {
+        "username": username,
+        "discipline": discipline,
+        "rating": rating,
+        "comment": comment
+    }
+    ReviewService.add_review(review)
+
+@when(
+    parsers.cfparse('uma requisição POST é enviada "{url}" com username "{username}", disciplina "{discipline}", nota "{rating}" e comentário "{comment}"'),
+    target_fixture="context"
+)
+def add_review(client, context, url: str, username: str, discipline: str, rating: int, comment: str):
+    # try route
+    response = client.post(url, json={"username": username, "discipline": discipline, "rating": rating, "comment": comment})
+    context["response"] = response
+
+    # remove review if it exists
+    existing_reviews = ReviewService.get_reviews_by_name_and_discipline(discipline, username)
+    if(len(existing_reviews) != 0):
+        id_to_delete = existing_reviews[0]['_id']
+        removed_id = ReviewService.delete_review(id_to_delete)
+    return context
+
+@then(
+    parsers.cfparse('o código da resposta é "{response_code}"'),
+    target_fixture="response_code"
+)
+def check_response_status_code(context, response_code: str):
+    assert context["response"].status_code == int(response_code)
+
+@then(parsers.cfparse('o JSON da resposta deve conter a mensagem "{message}"'))
+def response_json(context, message: str):
+    response_json = context["response"].json()
+    assert response_json["message"] == message
