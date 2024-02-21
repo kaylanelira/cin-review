c8e336404e6b19070e8e2efad01600551521c3b2
Caio Braga <caio.chab@gmail.com>
2024-02-21 19:41:48 -0300

    Backend tests discipline

--- backend/src/schemas/discipline.py
+++ backend/src/schemas/discipline.py
@@ -7,3 +7,15 @@ class Discipline(BaseModel):
     semester: int
     professor: str
     description: str
+
+
+    def is_complete(self) -> bool:
+        required_fields = ['name', 'code', 'department', 'semester', 'professor', 'description']
+        for field in required_fields:
+            field_value = getattr(self, field, None)
+            if isinstance(field_value, str):
+                if field_value.strip() == '':
+                    return False
+            elif field_value is None:
+                return False
+        return True
